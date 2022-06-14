79366cd3062a21c375978e8cdfb580c56568665d
Daniel <dm.white@gmx.com>
2022-06-14 03:44:12 +0100

    Internet users visualisation development

--- internet-users-by-country.js
+++ internet-users-by-country.js
@@ -8,6 +8,15 @@ function SearchBar() {
     let queryLength;
     let countries;
     let top5 = '';
+    this.xAxisLabel = 'year';
+    this.yAxisLabel = '% of population using internet services';
+    var marginSize = 35;
+
+    this.minUsers = 0;
+    this.maxUsers = 100;
+
+    this.startYear = 2010;
+    this.endYear = 2020;
     
 
     // Preload the data. This function is called automatically by the
@@ -21,8 +30,18 @@ function SearchBar() {
             function(table) {
                 self.loaded = true;
             });
-        
     }
+
+
+    this.setup = function() {
+        // Font defaults.
+        textSize(16);
+    
+        // Set min and max years: assumes data is sorted by date.
+        this.startYear = 2010
+        this.endYear = 2020
+    };
+
 
     this.drawTitle = function() {
         fill(0);
@@ -30,13 +49,17 @@ function SearchBar() {
         textAlign('center', 'center');
         textSize(25)
         text(this.title, width/2, height/40);
-    }
+    };
+
 
     this.createSearchBar = function() {
         // search bar label
-        textSize(16);
-        text('Search for your desired country', width/8, height/8);
-
+        textSize(18);
+        strokeWeight(0.1)
+        strokeCap(ROUND)
+        stroke(0,0,152);
+        text('Search for your desired country!', width/7.5, height/8);
+        
         // render the search bar to the screen
         if (searchToken == true) {
             inputBox = createInput('');
@@ -44,7 +67,6 @@ function SearchBar() {
             inputBox.size(215);
             searchToken = false; // ensures the input box and button are only drawn once, allowing text input in the box to persist
         }
-
 
         // save the search query to a variable
         let query = inputBox.value();
@@ -54,7 +76,8 @@ function SearchBar() {
             this.searchForQuery(query)
             queryLength = query.length
         }   
-    }
+    };
+
 
     // takes the search query as an input and filters matches from the countries column
     this.searchForQuery = function(searchQuery) {
@@ -65,14 +88,91 @@ function SearchBar() {
         }
         const resultsArray = filterCountries(countries, searchQuery);
         top5 = resultsArray.slice(0, 5);  
-    }
+    };
+
 
 
     // renders the first (maximum of 5) results from resultsArray to the screen
     this.displayResults = function(matches){
-        textAlign('left');
-        matches.forEach((element, index) => text(element, width/50, height/12 + 100 + index*55));
-    }
+        let self = this;
+        noStroke();
+        strokeWeight(0.5);
+
+        //function which generates each result and detects user activity on the result
+        const buildResultButton = function(el, i){
+            let r = [width/65, height/4.5 + i*55, width/4.5, height/15]
+            stroke(0)
+            if (mouseX > r[0] && mouseX < r[0] + r[2] && mouseY > r[1] && mouseY < r[1] + r[3]) {
+                fill(152, 255, 152);
+                if (mouseIsPressed){
+                    self.generateGraph(el);
+                }
+            } else {
+                noFill();
+            }
+            rect(r[0], r[1], r[2], r[3], 10);
+            noStroke();
+            fill(0)
+            text(el, width/8, height/12 + 100 + i*55)
+        }
+
+        textAlign('center');
+        matches.forEach((element, index) => buildResultButton(element, index));
+    };
+
+
+    this.layout = {
+        marginSize: marginSize,
+    
+        // Locations of margin positions. Left and bottom have double margin
+        // size due to axis and tick labels.
+        leftMargin: marginSize * 10,
+        rightMargin: width - marginSize,
+        topMargin: marginSize*2,
+        bottomMargin: height - marginSize * 2,
+        pad: 5,
+    
+        plotWidth: function() {
+          return this.rightMargin - this.leftMargin;
+        },
+    
+        plotHeight: function() {
+          return this.bottomMargin - this.topMargin;
+        },
+    
+        // Boolean to enable/disable background grid.
+        grid: false,
+    
+        // Number of axis tick labels to draw so that they are not drawn on
+        // top of one another.
+        numXTickLabels: 10,
+        numYTickLabels: 10,
+    };
+
+
+    this.mapYearToWidth = function(value) {
+        return map(value,
+                   this.startYear,
+                   this.endYear,
+                   this.layout.leftMargin,   // Draw left-to-right from margin.
+                   this.layout.rightMargin);
+    };
+
+
+    this.mapUsersToHeight = function(value) {
+        return map(value,
+                   this.minUsers,
+                   this.maxUsers,
+                   this.layout.bottomMargin, // Smaller pay gap at bottom.
+                   this.layout.topMargin);   // Bigger pay gap at top.
+    };
+
+
+    this.generateGraph = function(accessionKey) {
+        let row = this.data.findRow(accessionKey, 'Country Name');
+        console.log(row);
+    };
+
 
 
     this.draw = function() {
@@ -93,9 +193,29 @@ function SearchBar() {
             this.displayResults(top5);
         }
 
+        // Draw all y-axis labels.
+        drawYAxisTickLabels(this.minUsers,
+            this.maxUsers,
+            this.layout,
+            this.mapUsersToHeight.bind(this),
+            0);
+
+        // Draw x and y axis.
+        drawAxis(this.layout);
+
+        // Draw x and y axis labels.
+        drawAxisLabels(this.xAxisLabel,
+            this.yAxisLabel,
+            this.layout);
+
+        for (let i=0; i<11; i++){
+            drawXAxisTickLabel(i+1 + 2010, this.layout,
+                this.mapYearToWidth.bind(this));
+        }
+
         this.destroy = function() {
             inputBox.remove();
             searchToken = true; // required to ensure the search bar will redraw if user clicks back on visual again
         }
-    }
+    };
 }
